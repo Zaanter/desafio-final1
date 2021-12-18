ae8836f4f667fdd3ade711c38a4d306627c0e9cc
Agustin Troche <agustin.troche@everis.nttdata.com>
2021-12-18 13:39:57 -0300

    changing3

--- app.js
+++ app.js
@@ -13,9 +13,10 @@ app.post('/submit', function(req,res){
       "Content-Type":"application/json",
       "Accept": "*/*"
     }
-    axios.post('http://172.21.188.211:3000/submit', req.body,headers,{timeout:15000}).catch((error) => {
+    response = axios.post('http://172.21.188.211:3000/submit',req.body,{headers,timeout:15000}).catch((error) => {
           console.log('Error:' , error)
       })
+    res.json(response)
 })
 
 app.listen(8080, function() {
