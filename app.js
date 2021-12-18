012366e3fac6bf2ff15dbe1ceadcfc2b077853d2
Agustin Troche <agustin.troche@everis.nttdata.com>
2021-12-18 13:54:19 -0300

    testing

--- app.js
+++ app.js
@@ -14,7 +14,14 @@ app.post('/submit', function(req,res){
       "Content-Type":"application/json",
       "Accept": "*/*"
     }
-    response = axios.post('http://172.21.188.211:3000/submit',data).catch((error) => {
+    response = axios.post('http://172.21.188.211:3000/submit',{
+      "email": "agustrocheok@email.com",
+      "assistantId": "9069f9e4-1a59-409b-9134-fb559f1750e8",
+      "url": "https://api.us-south.assistant.watson.cloud.ibm.com/instances/c97fe366-1519-4451-8f16-91b7cf71c2cc",
+      "skillId": "d16e8fed-8588-49c7-8cae-e99d07a50137",
+      "apiKey": "ZTN3628mIE3nXW30aFR0fhtvGwZKSzFLBEr_5sZi_2NW",
+      "submitConfirmation": false
+    }).catch((error) => {
           console.log('Error:' , error)
       })
     res.json(response)
